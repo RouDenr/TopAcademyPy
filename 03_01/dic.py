86303d938658cd6b78bfc56e46449c9f8f5a4e3e
Dennet Cordelia <36990341+RouDenr@users.noreply.github.com>
2023-03-06 13:54:58 +0000

    dict

---
+++ 03_01/dic.py
@@ -0,0 +1,31 @@
+arr = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+# print(arr[0])
+# # print(arr[1])
+# # print(arr[2])
+
+
+
+# for i in arr :
+#     for j in i :
+#         print(j)
+
+test_dict = {
+    "Mike"  : 42,
+    "Tod"   : 21,
+    "Ron"   : 21
+}
+
+# print(test_dict)
+
+# for i in test_dict.keys() :
+#     print(i)
+# for i in test_dict :
+#     print(i)
+
+for i in test_dict.values() :
+    print(i)
+
